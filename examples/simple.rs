48269b5de3a9a0691483dc01b7caa00b1d6044de
Misha Wagner <mishajw@gmail.com>
2018-07-14 21:10:18 +0100

    Add simple example of library usage

---
+++ examples/simple.rs
@@ -0,0 +1,36 @@
+extern crate periodic;
+
+use std::time::{Duration, Instant};
+
+fn main() {
+    let mut planner = periodic::Planner::new();
+    planner.start();
+
+    // Run every second
+    planner.add(
+        &|| println!("every second"),
+        periodic::period::Every::new(Duration::from_secs(1)),
+    );
+
+    // Run after three seconds
+    planner.add(
+        &|| println!("after three seconds"),
+        periodic::period::After::new(Duration::from_secs(3)),
+    );
+
+    // Run at three, five, and seven seconds
+    planner.add(
+        &|| println!("after three, five, seven seconds"),
+        vec![
+            Duration::from_secs(3),
+            Duration::from_secs(5),
+            Duration::from_secs(7),
+        ],
+    );
+
+    // Can also pass `std::time::Instant`s instead of `Duration`s
+    planner.add(
+        &|| println!("after three seconds"),
+        Instant::now() + Duration::from_secs(3),
+    );
+}
