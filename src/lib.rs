dc0af7d521423294f2c96e0228aeb8ae9f37b68b
Misha Wagner <mishajw@gmail.com>
2018-07-15 20:22:47 +0100

    Improve crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
-//! Scheduling tool for running tasks at fixed intervals
+//! Simple scheduling tool for running tasks at fixed intervals.
+//!
+//! Handles all threads for scheduling and running tasks. Note that callbacks
+//! passed in must be able to execute asynchronously, and therefore require
+//! traits `Fn` (not `FnMut`), `Sync`, and `Send`. They must also have a
+//! `'static` lifetime.
+//!
+//! # Example usage
+//! ```no_run
+//! use std::time::Duration;
+//!
+//! let mut planner = periodic::Planner::new();
+//! planner.add(
+//!     || println!("every three seconds"),
+//!     periodic::period::Every::new(Duration::from_secs(3)),
+//! );
+//! planner.start();
+//! ```
+//!
+//! See `./examples` for more detailed usage.
 
 #![warn(missing_docs)]
 
